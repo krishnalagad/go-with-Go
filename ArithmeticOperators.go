fefce5bee574e45eb10aa7e5a2279ffe64498dab
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-07 23:27:00 +0530

    Update ArithmeticOperators.go

--- ArithmeticOperators.go
+++ ArithmeticOperators.go
@@ -3,11 +3,17 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	var num1 float32 = 90
+	var num1 float64 = 90
 	var num2 int = 8
-	ans := num1 / float32(num2)
-	fmt.Printf("%g", ans)
+	ans := num1 / float64(num2)
+	fmt.Printf("%.3g", math.Sqrt(ans))
+
+	var num3 float64 = 90
+	var num4 int = 8
+	res := num3 / float64(num4)
+	fmt.Printf("\n%.3g", math.Abs(res))
 }
