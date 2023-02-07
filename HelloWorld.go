14f08e59c2fa2b2755687fce0956e05b2c17b41a
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-07 19:32:14 +0530

    Update HelloWorld.go

--- HelloWorld.go
+++ HelloWorld.go
@@ -1,8 +1,9 @@
+// #1
+// Go is compiled programming language.
 package main
 
-import "fmt"  		// fmt module use to print messege to console.
+import "fmt" // fmt module use to print messege to console.
 
 func main() {
 	fmt.Println("Hello world!")
 }
-
