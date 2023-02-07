a422a4d65503505352cd847cb69a54dd169b280e
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-07 21:26:13 +0530

    Update FmtConsole.go

--- FmtConsole.go
+++ FmtConsole.go
@@ -4,5 +4,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Printing console")
+	fmt.Printf("KRISHNA %T %v", 10, 10)
+	var result string = fmt.Sprintf("KRISHNA %T %v", 10, 10)
+	fmt.Println(result)
+
+	fmt.Println()
+
+	fmt.Printf("Number binary: %b ", 345)
+	fmt.Printf("Number octal: %o ", 345)
+	fmt.Printf("Number decimal: %d ", 345)
+	fmt.Printf("Number hexodecimal: %x ", 3435)
 }
