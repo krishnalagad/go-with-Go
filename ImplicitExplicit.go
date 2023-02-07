6369f78ac85f1beee6b05199fa5373f6ff8343ab
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-07 19:53:36 +0530

    - datatypes in go

---
+++ ImplicitExplicit.go
@@ -0,0 +1,14 @@
+// #3
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Implicit and explicit")
+
+	var name = "Krishna Lagad" // implicit datatype
+	fmt.Println(name)
+
+	var mob = 9834765434
+	fmt.Println(mob)
+}
--- VariableDataTypes.go
+++ VariableDataTypes.go
@@ -6,6 +6,15 @@ import "fmt"
 func main() {
 	fmt.Println("Variables and Data Types")
 
-	var name string = "Krishna Lagad"
-	fmt.Println(name)
+	var name string = "Krishna Lagad" // explicitly specifying datatype
+	fmt.Println("My name is", name)
+
+	var age uint8 = 22
+	fmt.Println(age)
+
+	var mob uint64 = 9876452345
+	fmt.Println(mob)
+
+	var isTrue bool = false
+	fmt.Println(isTrue)
 }
