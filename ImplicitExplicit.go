60afe37ca91448b3fe2b73a3bd83a63febceb08a
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-07 20:04:12 +0530

    Update ImplicitExplicit.go

--- ImplicitExplicit.go
+++ ImplicitExplicit.go
@@ -11,4 +11,25 @@ func main() {
 
 	var mob = 9834765434
 	fmt.Println(mob)
+	fmt.Printf("%T", mob)
+
+	city := "Pune" // another way of declaring variables.
+	fmt.Println(city)
+	fmt.Printf("%T", city)
+
+	fmt.Println()
+
+	fmt.Println("Default values of variables...")
+
+	var number uint
+	fmt.Println(number)
+
+	var country string
+	fmt.Println(country)
+
+	var isTrue bool
+	fmt.Println(isTrue)
+
+	var percent float64
+	fmt.Println(percent)
 }
