232dbe5f00b84b76fb133508abf1e1287045dc00
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-08 14:40:16 +0530

    Create IfElse.go

---
+++ IfElse.go
@@ -0,0 +1,17 @@
+// #8
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	name := "krishna"
+
+	fmt.Println("Before if")
+	if name == "krishna" {
+		fmt.Println("Inside if")
+	}
+	fmt.Println("After if")
+}
