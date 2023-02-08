4bc9ee9c74923d4bb727a6212513ab9861744073
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-08 16:05:11 +0530

    Update Switch.go

--- Switch.go
+++ Switch.go
@@ -5,5 +5,15 @@ package main
 import "fmt"
 
 func main() {
-	
+	ans := 19
+	switch ans {
+	case 1:
+		fmt.Println("One")
+		break
+	case 2:
+		fmt.Println("Two")
+		break
+	default:
+		fmt.Println("not a case")
+	}
 }
