be5959dd3206776e5a474dc06e792520587844f1
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-09 13:22:13 +0530

    Update 17_AdvancedFunctions.go

--- 17_AdvancedFunctions.go
+++ 17_AdvancedFunctions.go
@@ -9,4 +9,16 @@ func test1(x int){
 func main() {
 	x := test1
 	x(34)
+
+	// function within function
+	fun := func(x int){
+		fmt.Println(x)
+	}
+	fun(56)
+
+	// passing argument to function while defining it.
+	test2 := func(x int) int{
+		return x * -1
+	}(78)
+	fmt.Println(test2)
 }
