00da6f6e43d72ca595f4068aac3477c727624b64
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-09 16:45:53 +0530

    Update 20_Structs.go

--- 20_Structs.go
+++ 20_Structs.go
@@ -7,8 +7,16 @@ type Point struct {
 	y int32
 }
 
+type Circle struct {
+	radius float32
+	center Point
+}
+
 func main() {
 	p1 := Point{1, 2}
 	p2 := Point{x: 3}
 	fmt.Println(p1, p2)
+
+	c1 := Circle{4.56, p1}
+	fmt.Println(c1)
 }
