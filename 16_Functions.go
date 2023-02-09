d010abf32806a82fcf9532f884ec50961142abc9
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-09 13:08:09 +0530

    Update 16_Functions.go

--- 16_Functions.go
+++ 16_Functions.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func test(){
+func test() {
 	fmt.Println("Test function !!")
 }
 
@@ -10,7 +10,27 @@ func getLength(str string) {
 	fmt.Println(len(str))
 }
 
+func getSum(a, b int) { // (a int, b int)
+	fmt.Println(a + b)
+}
+
+func mulName(name string, no int) {
+	for i := 0; i < no; i++ {
+		fmt.Printf("%s ", name)
+	}
+}
+
+func mulReturns(a, b int) (r1 int, r2 int) {
+	r1 = a + b
+	r2 = a - b
+	return 
+}
+
 func main() {
 	test()
 	getLength("Krishna")
+	getSum(12, 45)
+	mulName("Krishna", 10)
+	ans1, ans2 := mulReturns(5, 3)
+	fmt.Println(ans1, ans2)
 }
