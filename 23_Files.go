b0f76565c6eda3a987f1275b75d29477dbebf975
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-13 23:31:28 +0530

    - writing to file

--- 23_Files.go
+++ 23_Files.go
@@ -3,14 +3,17 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
+
+
 
 func main() {
 	fmt.Println("File Handling in Golang")
 
 	content := "This content need to go in file - Krishna Lagad is my name."
-	file, err := os.Create("./myfile.docx")
+	file, err := os.Create("./myfile.txt")
 
 	if err != nil {
 		panic(err)
@@ -22,4 +25,5 @@ func main() {
 	}
 	fmt.Println("Length: ", len)
 	defer file.Close()
+	readFile("./myfile.txt")
 }
