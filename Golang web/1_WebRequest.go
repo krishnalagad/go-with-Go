326a04082e3625f13a180b28e803c45bd7bc6bf9
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-13 23:47:44 +0530

    Update 1_WebRequest.go

--- Golang web/1_WebRequest.go
+++ Golang web/1_WebRequest.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+const url = "https://lco.dev"
+
 func main() {
-	
+	fmt.Println("Go web request")
+
+	response, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Response type is: %T", response)
+
+	response.Body.Close()	// user's responsibility to close the connection.
 }
