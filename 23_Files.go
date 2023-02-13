aeca7017b5179af02892e783f7f99d022a5508a7
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-13 23:26:40 +0530

    - file handling in golang.

--- 23_Files.go
+++ 23_Files.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
 	fmt.Println("File Handling in Golang")
-	
+
+	content := "This content need to go in file - Krishna Lagad is my name."
+	file, err := os.Create("./myfile.docx")
+
+	if err != nil {
+		panic(err)
+	}
+	len, err := io.WriteString(file, content)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Length: ", len)
+	defer file.Close()
 }
