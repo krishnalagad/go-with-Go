7affa0687526a9ebb21b827c7152f1d32a794ad8
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-14 00:42:27 +0530

    Update 2_Url.go

--- Golang web/2_Url.go
+++ Golang web/2_Url.go
@@ -1,11 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+)
 
-const myurl string = ""
+const myurl string = "https://lco.dev:3000/learn?coursename=reactjs&paymentid=jkadfhsd56"
 
 func main() {
 	fmt.Println("Golang URLs")
+	fmt.Println(myurl)
 
+	result, err := url.Parse(myurl)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result.Scheme)
+	fmt.Println(result.Port())
+	fmt.Println(result.Host)
+	fmt.Println(result.Path)
+	fmt.Println(result.RawQuery)
 
+	qparams := result.Query()
+	fmt.Printf("\nType of quesry param: %T\n", qparams)
+	fmt.Println(qparams["coursename"])
+
+	for _, val := range qparams {
+		fmt.Println(val)
+	}
+
+	// build url
+	partsOfUrl := &url.URL{
+		Scheme:   "https",
+		Host:     "localhost",
+		Path:     "/learn",
+		RawQuery: "user=krishna",
+	}
+	fmt.Println(partsOfUrl.String())
 }
