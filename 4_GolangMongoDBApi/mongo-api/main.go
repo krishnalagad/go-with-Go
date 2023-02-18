3f22e3f118c8d62852e8b9262f7bdcdead45c701
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-18 21:46:42 +0530

    - Defined port number.

--- 4_GolangMongoDBApi/mongo-api/main.go
+++ 4_GolangMongoDBApi/mongo-api/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/krishnalagad/mongo-api/router"
+)
 
 func main() {
-	fmt.Println("Project started...")
+	fmt.Println("Project started")
+	fmt.Println("Server is getting started...")
+
+	r := router.Router()
+	log.Fatal(http.ListenAndServe(":4000", r))
+
+	fmt.Println("Listening to port 4000")
+
 }
