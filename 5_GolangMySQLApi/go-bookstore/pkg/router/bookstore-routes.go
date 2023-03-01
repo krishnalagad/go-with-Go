2f294161fdb11fd8f715faa569714743ce28f761
Krishna Lagad <krishnalagad2@gmail.com>
2023-03-02 00:45:28 +0530

    - creating project, insralling required packages and defining routers.
    -  packages for MySQL, Gorilla mux etc..

---
+++ 5_GolangMySQLApi/go-bookstore/pkg/router/bookstore-routes.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/krishnalagad/go-bookstore/pkg/controllers"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/api/book", controllers.CreateBook). Methods("POST")
+	router.HandleFunc("/api/book", controllers.GetBooks). Methods("GET")
+	router.HandleFunc("/api/book/{id}", controllers.GetBookById). Methods("GET")
+	router.HandleFunc("/api/book/{id}", controllers.UpdateBook). Methods("PUT")
+	router.HandleFunc("/api/book/{id}", controllers.DeleteBook). Methods("DELETE")
+}
