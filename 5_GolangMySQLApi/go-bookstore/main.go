8d881070bbf74001b41d56402445824f9de7535e
Krishna Lagad <krishnalagad2@gmail.com>
2023-03-02 01:12:16 +0530

    - Dump all code of bookstore. Incomplete.

--- 5_GolangMySQLApi/go-bookstore/main.go
+++ 5_GolangMySQLApi/go-bookstore/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/krishnalagad/go-bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
