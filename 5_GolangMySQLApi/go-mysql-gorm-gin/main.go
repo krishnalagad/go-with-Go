1c19e3a299e99d0869de1f10645e271cca1223b1
Krishna Lagad <krishnalagad2@gmail.com>
2023-04-27 16:42:55 +0530

    - added controllers, model and configure database

--- 5_GolangMySQLApi/go-mysql-gorm-gin/main.go
+++ 5_GolangMySQLApi/go-mysql-gorm-gin/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+	"github.com/krishnalagad/go-mysql-gorm-gin/controllers"
+)
+
+func main() {
+	r := setupRouter()
+	_ = r.Run(":8080")
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "Working File !!")
+	})
+
+	userRepo := controllers.New()
+	r.POST("/users", userRepo.CreateUser)
+	r.GET("/users", userRepo.GetUsers)
+	r.GET("/users/:id", userRepo.GetUser)
+	r.PUT("/users/:id", userRepo.UpdateUser)
+	r.DELETE("/users/:id", userRepo.DeleteUser)
+
+	return r
 }
